0f3c75ebc3e834c6d41837b956ad37eb5adeae4a
Gerum-Berhanu <gerumberhanu0@gmail.com>
2024-05-28 23:42:34 +0300

    Perfeclty works (headless=False)

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from playwright.sync_api import sync_playwright
 import time
 
-playwright = sync_playwright().start()
+def run():
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    url = "https://chatgpt.com"
+    page.goto(url)
+    page.wait_for_selector("textarea#prompt-textarea")
+    textarea = page.locator("textarea#prompt-textarea")
+    button = page.locator("button.absolute.bottom-1\\.5")
 
-browser = playwright.chromium.launch(headless=False)
-page = browser.new_page()
-url = "https://chatgpt.com"
-page.goto(url)
+    def message():
+        prompt = input("Ask PlayAI: ")
+        while not prompt:
+            prompt = input("Ask PlayAI: ")
+        textarea.fill(prompt)
+        time.sleep(1)
+        button.click()
+        page.wait_for_selector("button.rounded-md.p-1.text-token-text-tertiary")
+        elements = page.locator(".markdown > p:nth-child(1)").all()
+        last_response = elements[-1].text_content()
+        return last_response
 
-textarea = page.get_by_placeholder("Message ChatGPT")
-button = page.locator("button.absolute.bottom-1\.5")
+    result = message()
+    print(result)
+    browser.close()
 
-def message():
-    prompt = input("Ask ChatGPT: ")
-    while not prompt:
-        prompt = input("Ask ChatGPT: ")
-    textarea.fill(prompt)
-    time.sleep(1)
-    button.click()
-    time.sleep(10)
-    elements = page.locator(".markdown > p:nth-child(1)").all()
-    last_response = elements[-1].text_content()
-    return last_response
-
-result = message()
-print(result)
-
-browser.close()
+if __name__ == "__main__":
+    run()
